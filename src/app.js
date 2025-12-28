7acabf0d8e763ae56b94a8c7db68992d04a0d9ac
Fourvenues-webs <webdev@fourvenues.com>
2025-12-28 20:44:32 +0100

    .

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 const recipeRoutes = require('./routes/recipeRoutes')
 
